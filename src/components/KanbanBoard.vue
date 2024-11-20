274a80e47d209b85a95cc956ceb2eb56b77ea1ca
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2024-11-20 23:37:48 +0300

    feat: added floating div

--- src/components/KanbanBoard.vue
+++ src/components/KanbanBoard.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="kanban-board">
-    <div v-for="(column, index) in columns" :key="index" class="kanban-column">
-      <div class="kanban-column-block">
+    <div v-for="(column, indexColumn) in visibledColumns" :key="indexColumn" class="kanban-column">
+      <div :class="['kanban-column-block']" :data-index="indexColumn">
         <h2 class="kanban-column-title">{{ column.title }}</h2>
         <ul class="kanban-column-list">
-          <li v-for="(task, index) in column.tasks" :key="task + index" class="kanban-task">
+          <li
+            v-for="(task, index) in column.tasks"
+            :key="task + index"
+            class="kanban-task"
+            @mousedown="startDrag(column, index, indexColumn, $event)"
+          >
             {{ task }}
           </li>
         </ul>
+        <div v-if="draggedTask" :style="floatingStyle" class="kanban-task">
+          {{ draggedTask }}
+        </div>
       </div>
     </div>
   </div>
@@ -15,9 +23,68 @@
 
 <script setup lang="ts">
 import type { Column } from '@/types/KanbanBoard'
-import { defineProps } from 'vue'
+import { computed, defineProps } from 'vue'
+import { ref, reactive } from 'vue'
 
-defineProps<{ columns: Column[] }>()
+const props = defineProps<{ columns: Column[] }>()
+
+const draggedTask = ref<string | null>(null)
+const cursorPosition = reactive({ x: 0, y: 0 })
+const visibledColumns = ref<Column[]>(props.columns)
+const currentColumns = ref<number | null>(null)
+
+const floatingStyle = computed(() => ({
+  position: 'absolute' as const,
+  top: `${cursorPosition.y}px`,
+  left: `${cursorPosition.x}px`,
+  pointerEvents: 'none' as const,
+  zIndex: 1000,
+}))
+
+const startDrag = (column: Column, index: number, indexColumn: number, event: MouseEvent) => {
+  draggedTask.value = column.tasks[index]
+  currentColumns.value = indexColumn
+  updateCursorPosition(event)
+
+  const copyColumns = JSON.parse(JSON.stringify(visibledColumns.value))
+
+  visibledColumns.value[currentColumns.value].tasks = copyColumns[
+    currentColumns.value
+  ].tasks.filter((el: string) => el !== draggedTask.value)
+
+  document.body.style.userSelect = 'none'
+
+  document.addEventListener('mousemove', onMouseMove)
+  document.addEventListener('mouseup', stopDrag)
+}
+
+const updateCursorPosition = (event: MouseEvent) => {
+  cursorPosition.x = event.clientX + 10
+  cursorPosition.y = event.clientY + 10
+}
+
+const onMouseMove = (event: MouseEvent) => {
+  updateCursorPosition(event)
+}
+const stopDrag = (event: MouseEvent) => {
+  const target = event.target as HTMLElement
+  const closestElement = target.closest('[data-index]') as HTMLElement
+
+  if (closestElement) {
+    const targetIndex = closestElement.dataset.index
+
+    if (targetIndex !== undefined && draggedTask.value) {
+      currentColumns.value = Number(targetIndex)
+      visibledColumns.value[currentColumns.value].tasks.push(draggedTask.value)
+    }
+  }
+
+  draggedTask.value = null
+  document.body.style.userSelect = ''
+
+  document.removeEventListener('mousemove', onMouseMove)
+  document.removeEventListener('mouseup', stopDrag)
+}
 </script>
 
 <style scoped>
@@ -31,29 +98,30 @@ defineProps<{ columns: Column[] }>()
   padding: 20px 0;
   border-right: 1px solid white;
   min-height: inherit;
-    width: 100%;
+  width: 100%;
   display: flex;
   justify-content: center;
 }
-.kanban-column-block{
+.kanban-column-block {
   width: 100%;
 }
-.kanban-column-title{
+.kanban-column-title {
   text-align: center;
   border-bottom: 1px solid white;
   margin-bottom: 20px;
 }
-.kanban-column-list{
+.kanban-column-list {
   display: flex;
   flex-direction: column;
   padding: 0 10px;
   list-style-type: none;
   gap: 10px;
 }
-.kanban-task{
+.kanban-task {
   padding: 10px;
-  cursor: pointer;
   border-radius: 10px;
   border: 1px solid white;
+  cursor: pointer;
+  user-select: none;
 }
 </style>
