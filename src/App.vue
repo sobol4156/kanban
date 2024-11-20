1f7951acff578b23c4ab35f9a64ef105a94711b8
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2024-11-20 22:17:57 +0300

    refactor: small redesign

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <header>
-
-  </header>
-
-  <main>
+  <main class="main">
     <KanbanBoard :columns="columns" />
   </main>
 </template>
@@ -12,37 +8,14 @@
 import KanbanBoard from '@/components/KanbanBoard.vue'
 
 const columns = [
-  { id: 1, title: 'To Do', tasks: [] },
-  { id: 2, title: 'In Progress', tasks: [] },
-  { id: 3, title: 'Done', tasks: [] },
+  { id: 1, title: 'To Do', tasks: ['сделать то', 'сделать сё'] },
+  { id: 2, title: 'In Progress', tasks: ['делается'] },
+  { id: 3, title: 'Done', tasks: ['сделали'] },
 ]
 </script>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.main{
+  min-height: inherit;
 }
 </style>
--- src/components/KanbanBoard.vue
+++ src/components/KanbanBoard.vue
@@ -1,19 +1,59 @@
 <template>
   <div class="kanban-board">
     <div v-for="(column, index) in columns" :key="index" class="kanban-column">
-      <h2 class="kanban-column-title">{{ column.title }}</h2>
-      <ul class="kanban-column-list">
-        <li v-for="(task, index) in column.tasks" :key="task+index" class="kanban-task">
-          {{ task }}
-        </li>
-      </ul>
+      <div class="kanban-column-block">
+        <h2 class="kanban-column-title">{{ column.title }}</h2>
+        <ul class="kanban-column-list">
+          <li v-for="(task, index) in column.tasks" :key="task + index" class="kanban-task">
+            {{ task }}
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { Column } from '@/types/KanbanBoard';
-import { defineProps } from 'vue';
+import type { Column } from '@/types/KanbanBoard'
+import { defineProps } from 'vue'
 
-defineProps<{columns: Column[]}>()
+defineProps<{ columns: Column[] }>()
 </script>
+
+<style scoped>
+.kanban-board {
+  display: flex;
+  justify-content: space-around;
+  min-height: inherit;
+}
+.kanban-column {
+  height: 100%;
+  padding: 20px 0;
+  border-right: 1px solid white;
+  min-height: inherit;
+    width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.kanban-column-block{
+  width: 100%;
+}
+.kanban-column-title{
+  text-align: center;
+  border-bottom: 1px solid white;
+  margin-bottom: 20px;
+}
+.kanban-column-list{
+  display: flex;
+  flex-direction: column;
+  padding: 0 10px;
+  list-style-type: none;
+  gap: 10px;
+}
+.kanban-task{
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 10px;
+  border: 1px solid white;
+}
+</style>
